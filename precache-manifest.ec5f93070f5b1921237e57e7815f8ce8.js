7e0a4a8fcdcc1d316a1718754a8dfd4412f405e1
sh1tanshu@gmail.com <sh1tanshu@gmail.com>
2019-06-17 10:19:22 +0530

    Updates

---
+++ precache-manifest.ec5f93070f5b1921237e57e7815f8ce8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "59c6786fff0de8e8f44c",
+    "url": "/static/css/main.6417cdd8.chunk.css"
+  },
+  {
+    "revision": "59c6786fff0de8e8f44c",
+    "url": "/static/js/main.59c6786f.chunk.js"
+  },
+  {
+    "revision": "70395b8b40c5adfe2066",
+    "url": "/static/js/1.70395b8b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e327160ba23d1bc7c457b63798b85a69",
+    "url": "/static/media/camera-coffee-composition-1509428.e327160b.jpg"
+  },
+  {
+    "revision": "00fca81fea7d105ec77d5675acd22e7f",
+    "url": "/static/media/hero.00fca81f.jpg"
+  },
+  {
+    "revision": "ed8e7e9ffc807fee38edfa48ceb9313e",
+    "url": "/static/media/analysis-brainstorming-1661004F.ed8e7e9f.jpg"
+  },
+  {
+    "revision": "9c96686603386ee1696aa60a0658bccc",
+    "url": "/static/media/who-hero.9c966866.jpg"
+  },
+  {
+    "revision": "eeb2676c67fc8a88a890339ab5c23100",
+    "url": "/static/media/cardimage1.eeb2676c.jpg"
+  },
+  {
+    "revision": "2c0aa2ae6d34fde569417306bb53e58b",
+    "url": "/static/media/cardimage2.2c0aa2ae.jpg"
+  },
+  {
+    "revision": "8a0a53deee5cc9a68116ab2140b3c0f5",
+    "url": "/static/media/cardimage3.8a0a53de.jpg"
+  },
+  {
+    "revision": "57017de0759c4507599ea956f4cd609a",
+    "url": "/static/media/bg-01.57017de0.jpg"
+  },
+  {
+    "revision": "46d3371166d5437ce1f85d26d719fded",
+    "url": "/index.html"
+  }
+];
